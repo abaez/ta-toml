05319e383ab60174f51ab53d11da5f2aa8835ac1
Alejandro Baez <Alejan.Baez@gmail.com>
2015-03-06 17:59:03 -0500

    lexer: change module call to local

--- toml.lua
+++ toml.lua
@@ -5,7 +5,8 @@
 -- @license MIT (see LICENSE)
 -- @module toml
 
-local l, token, word_match = lexer, lexer.token, lexer.word_match
+local l = require("lexer")
+local token, word_match = l.token, l.word_match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
 local M = {_NAME = 'toml'}
